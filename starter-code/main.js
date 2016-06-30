7a81d4270bc1f81df6ccb69e1df7fe9a646853dc
Alexander Braun <Alex@Alexanders-MacBook-Pro.local>
2016-06-30 05:55:20 -0400

    created if else statement

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,4 +3,17 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "queen";
 
-console.log(cardOne == cardThree);
+
+if (cardTwo === cardFour) {
+	alert("Sorry, try again.")
+} else if (cardTwo === cardThree) {
+	alert("Sorry, try again.")
+} else if (cardTwo === cardOne) {
+	alert("You found a match!")
+} else if (cardOne === cardThree) {
+	alert("Sorry, try again.")
+} else if (cardOne === cardFour) {
+	alert("Sorry, try again.")
+} else if (cardThree === cardFour) {
+	alert("You found a match!")
+}
