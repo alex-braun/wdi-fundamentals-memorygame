2f6844a35519f15b68d144301701b96debc598f7
Alexander Braun <Alex@Alexanders-MacBook-Pro.local>
2016-07-07 17:39:37 -0400

    finished game not functioning

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,26 +3,41 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "queen";
 
-var pick = document.getElementById('game-board');
+/*var pick = document.getElementById('game-board');
 function createBoard () {
-	for (var i = 0; i <= 4; i++) {
+	for (var i = 1; i <= 4; i++) {
 		var newCard = document.createElement('div');
 		newCard[i] = newCard;
 		newCard.className = "card";
 		pick.appendChild(newCard);
 	}
 }
-console.log(createBoard());
-/*if (cardTwo === cardFour) {
+console.log(createBoard());*/
+
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+function createBoard () {
+	for (var i = 0; i < cards.length i++) {
+		document.cardElement.setAttribute('data-card', cards[i]);
+		document.cardElement.addEventListener('click', isTwoCards);
+		document.cardElement.innerHTML = '<img src="images/Hearts 12.png" alt="Queen of Hearts" />';
+	}
+}
+
+function isTwoCards () {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+function isMatch () {
+if (cardsInPlay[0] !== cardsInPlay[1]) {
 	alert("Sorry, try again.")
-} else if (cardTwo === cardThree) {
-	alert("Sorry, try again.")
-} else if (cardTwo === cardOne) {
+} else if (card[0] === card[1]) {
 	alert("You found a match!")
-} else if (cardOne === cardThree) {
-	alert("Sorry, try again.")
-} else if (cardOne === cardFour) {
-	alert("Sorry, try again.")
-} else if (cardThree === cardFour) {
-	alert("You found a match!")
-}*/
+}
+document.getElementByClassName('card').innerHTML = "";
+}
